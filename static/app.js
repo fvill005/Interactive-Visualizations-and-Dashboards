08baf7d070a8b5bc2a6857cbee78c84444680b56
Filiberto Villegas <filibertovillegas92@gmail.com>
2020-09-17 21:19:07 -0700

    fixed minor display issue

--- static/app.js
+++ static/app.js
@@ -1,36 +1,11 @@
-// d3.json("data/samples.json").then((importedData) => {
-//     //console.log(importedData);
-//     var data = importedData;
-//     console.log(data);
-
-// });
 
 
 
 function init() {
-    // Select the dropdown element
-    var selector = d3.select("#selDataset");
-    
-    // Populate the dropdown with subject ID's from the list of sample Names
-      d3.json("data/samples.json").then((data) => {
-        var subjectIds = data.names;
-        subjectIds.forEach((id) => {
-          selector
-          .append("option")
-          .text(id)
-          .property("value", id);
-        });
-      
-      // Use the first subject ID from the names to build initial plots
-      var firstSubject = subjectIds[0];
-      updateCharts(firstSubject);
-      updateMetadata(firstSubject);
-    });
-  }
   
-  
-  
-  function updateMetadata(sample) {
+    //reading in the data from the json file
+    //using oject.entries to add a key value an pair to the meta panel
+    function updateMetadata(sample) {
     d3.json("data/samples.json").then((data) => {
         var metadata = data.metadata;
         var filterArray = metadata.filter(sampleObject => sampleObject.id == sample);
@@ -41,15 +16,14 @@ function init() {
             metaPanel.append("h6").text(`${key.toUpperCase()}: ${value}`)
         })
     
- 
-  // Use `Object.entries` to add each key and value pair to the metaPanel
-  // Hint: Inside the loop, you will need to use d3 to append new
-  // tags for each key-value in the metadata.
     });
   }
   
   
   function updateCharts(sample) {    
+   //importing data from json file
+   //filtering the array and declaring var to build plots
+
     d3.json("data/samples.json").then((data) => {
     var samples = data.samples;
     var filterArray = samples.filter(sampleObject => sampleObject.id == sample);
@@ -57,7 +31,8 @@ function init() {
     var sample_values = result.sample_values;
     var otu_ids = result.otu_ids;
     var otu_labels = result.otu_labels;   
-    // Bubble Chart
+    
+    // declaring trace for bubble chart
     var trace1 = {
         x: otu_ids,
         y: sample_values,
@@ -66,41 +41,72 @@ function init() {
         marker: {
         size: sample_values,
         color: otu_ids,
-        colorscale:"Electric"
+        colorscale:"cyl"
         }
     };
     var data = [trace1];
+    //formatting chart with layout 
     var layout = {
-        title: 'Bacteria Cultures per Sample',
+        title: 'Bacteria per Sample',
         showlegend: false,
         hovermode: 'closest',
         xaxis: {title:"OTU ID " +sample},
         margin: {t:30}
     };
+    //building plot 
     Plotly.newPlot('bubble', data, layout); 
-    // Bar Chart
+    
+    //seting up trace for the horizontal bar chart 
+    //slicing the array since we only need top 10
+    //reversing labels so we can get results in descending order
+    //changing color of bar graph for fun
     var trace1 = {
         x: sample_values.slice(0,10).reverse(),
         y: otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
         text: otu_labels.slice(0,10).reverse(),
-        name: "Greek",
         type: "bar",
-        orientation: "h"
+        orientation: "h",
+        marker: {
+            color: 'rgb(100,200,189)',
+            opacity: 0.7,
+          }
+    
     };
     var data = [trace1];
     var layout = {
-        title: "Top Ten OTUs for Individual " +sample,
+        title: "Top Ten OTUs " +sample,
         margin: {l: 100, r: 100, t: 100, b: 100}
+       
     };
     Plotly.newPlot("bar", data, layout);  
     });
   }
   
-  
-  function optionChanged(newSample) {
-    // Fetch new data each time a new sample is selected
-    updateCharts(newSample);
-    updateMetadata(newSample);
+   // updating and fetching new data when a different value is selected in the drop down 
+  function dropdownmenu(newID) {
+   updateCharts(newID);
+    updateMetadata(newID);
+  }
+
+ 
+    // Select the dropdown element
+    var selector = d3.select("#selDataset");
+    
+    // add IDs to the dropdown from the list of sample Names
+      d3.json("data/samples.json").then((data) => {
+        var subject_Ids = data.names;
+        subject_Ids.forEach((id) => {
+          selector
+          .append("option")
+          .text(id)
+          .property("value", id);
+        });
+      
+      // Use the first subject ID from the names to build initial plots
+      var firstID = subject_Ids[0];
+      updateCharts(firstID);
+      updateMetadata(firstID);
+    });
   }
   
   // Initialize the dashboard
